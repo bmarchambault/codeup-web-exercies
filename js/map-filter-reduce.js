5b1cceb8f81411af508ba728560b79d9bf5291c1
Bobbie Archambault <bmarchambault@gmail.com>
2020-05-22 10:44:09 -0500

    created map filter redue exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -59,12 +59,26 @@ let totalYearsExperience = users.reduce((total, person) => total += person.years
 //     return total;
 // }
 
-
 //both of the above work
 
-// Use .reduce to get the longest email from the list of users.
-// let longestEmail = users.reduce((longestEmail, {user:email}) => (longestEmail.email.length > user.email.length) ? longestEmail: {user:email}, {email: 0});
-// console.log(longestEmail);
+// // Use .reduce to get the longest email from the list of users.
+    let longestEmail = users.reduce((longestEmail, user) => {
+        // return (longestEmail.length > user.email.length) ? longestEmail : user.email, ""
+        if (longestEmail.length > user.email.length) {
+            return longestEmail;
+        } else {
+            return user.email;
+        }
+    });
+
+    //
+    // let longestEmail = users.reduce((longestEmail, user) => (longestEmail.email.length > user.email.length)
+    // ? longestEmail : user, {email: 0})
+
+console.log(longestEmail);
+
+
+
 
 // Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 let namesInAString = users.reduce((nameString, user) => nameString + `${user.name}`, "");
@@ -72,5 +86,35 @@ let namesInAString = users.reduce((nameString, user) => nameString + `${user.nam
 
 //Bonus
 //Use .reduce to get the unique list of languages from the list of users.
+//
+// let uniqueLang = users.reduce((accumulator, currentValue) => {
+//      //this method take 3 things - accum, curr and startValue.
+//     // what data type am I returning:  -- returning an array.
+//     //whats the accum?---unique languages
+//     //whats the currValue? --the user
+//     //whats the starting value? -- empty array
+//     //what data are we trying to access: -- user.languages in an object array
+//      //how can we access that data? --iteration through object array
+//      //what methods do i know to cycle through or manipulate that data? for loop, forEach, for/of loop
+//      //are there comparisons?  if so, what to what?  -- words to words
+//      // how can i compare?  -- google -- MDN says:
+//              let myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd']
+//              let myOrderedArray = myArray.reduce(function (accumulator, currentValue) {
+//                  if (accumulator.indexOf(currentValue) === -1) {
+//                          accumulator.push(currentValue)
+//                   }
+//                  return accumulator
+//                  }, [])
+//      //what do i need to do?  -- look ea. user in the array.  look at their languages.  iterate through languages. if user.languages.length is greater than the accumulater, return an array of languages.
+//      //based on MDN solution, i need to account for the languages being in it own array.  whats the solves?  a for loop or for each or for/of
+// },[]);//starting value (bucket)
 
-let uniqueLang = users.reduce((uniqueLanguage, user) => (uniqueLanguage.languages === user.languages) ? uniqueLanguage : languages, {languages: })
+let uniqueLangsArray = users.reduce((uniqueLang, user) => {
+    for (let i = 0; i < user.languages.length; i += 1){
+        if (uniqueLang.indexOf(user.languages[i]) === -1) {
+             uniqueLang.push(user.languages[i]);
+         }
+    }
+    return uniqueLang;
+},[]);
+console.log(uniqueLangsArray);
