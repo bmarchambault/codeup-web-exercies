7dd06d4d60de559b131852f82ba611b7a661c342
Bobbie Archambault <bmarchambault@gmail.com>
2020-05-28 17:47:04 -0500

    redoing exercises for practice

--- study-files/js-study/exercise-doover.js
+++ study-files/js-study/exercise-doover.js
@@ -82,13 +82,15 @@ console.log(calculateTip(.20, 20))
  * prompt the user for the bill total and a percentage they would like to tip,
  * then display the dollar amount they should tip
  */
-const tipAmount = () => {
-    let getBillTotal = prompt("Please enter your total bill");
-    let getTip = prompt('Please enter the percent you would like to tip');
 
-return alert('You should tip $' + calculateTip(getTip, getBillTotal) + ' dollars.' )
-}
-tipAmount();
+// const tipAmount = () => {
+//     let getBillTotal = prompt("Please enter your total bill");
+//     let getTip = prompt('Please enter the percent you would like to tip');
+//
+// return alert('You should tip $' + calculateTip(getTip, getBillTotal) + ' dollars.' )
+// }
+// tipAmount();
+
 /**
  * TODO:
  * Create a function named `applyDiscount`. This function should accept a price
@@ -102,4 +104,140 @@ tipAmount();
  * > applyDiscount(originalPrice, dicountPercent) // 80
  *
  * > applyDiscount(45.99, 0.12) // 40.4712
+ */
+
+const applyDiscount = (originalPrice, discountPerecnt) => {
+    let discount = originalPrice * discountPerecnt;
+    return originalPrice - discount
+}
+console.log(applyDiscount(100, .2))
+
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message that related to that
+ * color. Only worry about the colors defined below, if the color passed is not
+ * one of the ones defined below, return a message that says so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your function and console.log the results.
+ * You should see a different message every time you refresh the page
+ */
+
+/**
+ * TODO:
+ * Refactor your above function to use a switch-case statement
+ */
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * all for free!.
+ *
+ * Write a function named `calculateTotal` that accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+
+// const calculateTotal = (luckyNumber, totalAmount) =>{
+//     let lucky5 = 0
+//     let lucky4 = .50 * totalAmount;
+//     let lucky3 = .35 * totalAmount;
+//     let lucky2 = .25 * totalAmount;
+//
+//     if(luckyNumber === 5){
+//         return lucky5;
+//     } else if (luckyNumber === 4){
+//         return totalAmount - lucky4;
+//     } else if (luckyNumber === 3){
+//         return totalAmount - lucky3;
+//     } else if (luckyNumber === 2){
+//         return totalAmount - lucky2;
+//     } else{
+//         return totalAmount;
+//     }
+// }
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 6.
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+// Generate a random number between 0 and 6
+var luckyNumber = Math.floor(Math.random() * 6);
+let getCustomerBill = prompt('What was your total bill?');
+const calculateTotal = (luckyNumber, getCustomerBill) =>{
+    let lucky5 = 0
+    let lucky4 = .50 * totalAmount;
+    let lucky3 = .35 * totalAmount;
+    let lucky2 = .25 * totalAmount;
+
+    if(luckyNumber === 5){
+        return lucky5;
+    } else if (luckyNumber === 4){
+        return totalAmount - lucky4;
+    } else if (luckyNumber === 3){
+        return totalAmount - lucky3;
+    } else if (luckyNumber === 2){
+        return totalAmount - lucky2;
+    } else{
+        return totalAmount;
+    }
+}
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * if what the user enters is not a number, use an alert to tell them that, and
+ * do *not* display any of the above information.
+ *
+ * Can you refactor your code to use functions?
+ * HINT: The way we prompt for a value could be improved
  */
